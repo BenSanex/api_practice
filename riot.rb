9127eb2eb07ef4c61db016e4125a297a9dfed86a
BenSanex <ben.sanecki@gmail.com>
2016-05-01 15:23:57 -0500

    removed api key to push to remote

--- riot.rb
+++ riot.rb
@@ -1,7 +1,7 @@
 require 'net/http'
 require 'json'
 
-RIOT_API_KEY = 'a61510fa-2726-4554-b394-4b24fb9ded2f'
+RIOT_API_KEY = ''
 uri = URI('https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/BenSanex?api_key=' + RIOT_API_KEY)
 
 raw_json = Net::HTTP.get(uri)
